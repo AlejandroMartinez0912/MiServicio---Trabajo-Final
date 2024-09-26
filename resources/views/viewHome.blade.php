b0f27dd90daa39041daef9bfdcd7ea57f6868c01
Alejandro Martinez <martinezalejandrouni10@gmail.com>
2024-09-26 14:59:43 -0300

    Avance en en el home

---
+++ resources/views/viewHome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla')
+
+@section('titulo')
+    MiServicio
+@endsection
+
+@section('body')
+    <h1>Hola mundo</h1>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+//Ruta de pagina princial llamada Home
+Route::get('/', [HomeController::class, 'index']);
+
